81423610744c6fcf2974869c2261e029755ae35e
CameronDeweerd <87665700+CameronDeweerd@users.noreply.github.com>
2021-10-20 10:46:36 -0400

    turned on debug mode so the code auto-updates when i change HTML

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 # The . makes these "relative imports" which is important to make them work on the server but technically doesn't matter here.
-from .helpers import apology
-from .issueTrackerViews import issueTrack
+from helpers import apology
+from issueTrackerViews import issueTrack
 
 app.register_blueprint(issueTrack)
 
@@ -31,4 +31,4 @@ app.register_error_handler(404, page_not_found)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
